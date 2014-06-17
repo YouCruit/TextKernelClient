fd9b2190c3ab5a46a9fd96aa91d76a93c4998137
christianekstrand <christianekstrand@gmail.com>
2014-06-17 13:29:40 +0200

    Added methods to get profilea via InputStream and URI.

--- src/main/java/com/youcruit/textkernel/client/TextKernelClient.java
+++ src/main/java/com/youcruit/textkernel/client/TextKernelClient.java
@@ -3,17 +3,21 @@ package com.youcruit.textkernel.client;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.entity.mime.FormBodyPart;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ContentBody;
 import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.InputStreamBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
@@ -47,17 +51,23 @@ public class TextKernelClient {
     public Profile getProfile(File file) throws JsonParseException, JsonMappingException, IOException, URISyntaxException{
 	return p.parse(upload(file));
     }
+    
+    public Profile getProfile(InputStream is) throws JsonParseException, JsonMappingException, ClientProtocolException, IOException, URISyntaxException{
+	return p.parse(upload(new InputStreamBody(is, "")));
+    }
+    
+    public Profile getProfile(URI uri) throws JsonParseException, JsonMappingException, MalformedURLException, IOException, URISyntaxException{
+	return getProfile(uri.toURL().openStream());
+    }
 
-    private InputStream upload(File file) throws IOException, URISyntaxException {
+    private InputStream upload(ContentBody content) throws ClientProtocolException, IOException, URISyntaxException{
 	DefaultHttpClient client = getClientInstance();
 	HttpContext localContext = new BasicHttpContext();
 	URI upload_url = new URI(textKernelUrl);
 	HttpPost upload = new HttpPost(upload_url);
 
 	MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-
-	FileBody fileBody = new FileBody(file, "application/octet-stream");
-	entity.addPart(FORM_PART_NAME_FILE, fileBody);
+	entity.addPart(FORM_PART_NAME_FILE, content);
 	entity.addPart(new FormBodyPart(FORM_PART_NAME_USERNAME, new StringBody(username)));
 	entity.addPart(new FormBodyPart(FORM_PART_NAME_ACCOUNT, new StringBody(account)));
 	entity.addPart(new FormBodyPart(FORM_PART_NAME_PASSWORD, new StringBody(password)));
@@ -66,6 +76,11 @@ public class TextKernelClient {
 
 	HttpEntity responseEntity = response.getEntity();
 	return responseEntity.getContent();
+    }
+    
+    private InputStream upload(File file) throws IOException, URISyntaxException {
+	FileBody fileBody = new FileBody(file, "application/octet-stream");
+	return upload(fileBody);
     }
 
     private DefaultHttpClient getClientInstance() {
