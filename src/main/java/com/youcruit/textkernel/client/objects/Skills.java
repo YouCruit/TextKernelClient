40449549cd775a72acc9287a863c111ec0c142b1
christianekstrand <christianekstrand@gmail.com>
2014-06-18 15:01:01 +0200

    Added getters, setters, equals, hashcode and toString

--- src/main/java/com/youcruit/textkernel/client/objects/Skills.java
+++ src/main/java/com/youcruit/textkernel/client/objects/Skills.java
@@ -11,4 +11,52 @@ public class Skills {
     private List<LanguageSkill> languageSkills;
     @JacksonXmlProperty(localName="SoftSkills")
     private List<SoftSkill> softSkills;
+    public List<ComputerSkill> getComputerSkills() {
+        return computerSkills;
+    }
+    public void setComputerSkills(List<ComputerSkill> computerSkills) {
+        this.computerSkills = computerSkills;
+    }
+    public List<LanguageSkill> getLanguageSkills() {
+        return languageSkills;
+    }
+    public void setLanguageSkills(List<LanguageSkill> languageSkills) {
+        this.languageSkills = languageSkills;
+    }
+    public List<SoftSkill> getSoftSkills() {
+        return softSkills;
+    }
+    public void setSoftSkills(List<SoftSkill> softSkills) {
+        this.softSkills = softSkills;
+    }
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((computerSkills == null) ? 0 : computerSkills.hashCode());
+	result = prime * result + ((languageSkills == null) ? 0 : languageSkills.hashCode());
+	result = prime * result + ((softSkills == null) ? 0 : softSkills.hashCode());
+	return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj) return true;
+	if (obj == null) return false;
+	if (!(obj instanceof Skills)) return false;
+	Skills other = (Skills) obj;
+	if (computerSkills == null) {
+	    if (other.computerSkills != null) return false;
+	} else if (!computerSkills.equals(other.computerSkills)) return false;
+	if (languageSkills == null) {
+	    if (other.languageSkills != null) return false;
+	} else if (!languageSkills.equals(other.languageSkills)) return false;
+	if (softSkills == null) {
+	    if (other.softSkills != null) return false;
+	} else if (!softSkills.equals(other.softSkills)) return false;
+	return true;
+    }
+    @Override
+    public String toString() {
+	return "Skills [computerSkills=" + computerSkills + ", languageSkills=" + languageSkills + ", softSkills=" + softSkills + "]";
+    }
 }
