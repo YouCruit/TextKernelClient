d0c5df7ec8aecfee0d6e0efb975a12158cb8cefd
Patrick Gilmore <p@noonday.se>
2015-10-29 11:27:04 +0100

    Reads file as input stream instead

--- src/test/java/com/youcruit/textkernel/client/TextKernelINTTest.java
+++ src/test/java/com/youcruit/textkernel/client/TextKernelINTTest.java
@@ -1,23 +1,27 @@
 package com.youcruit.textkernel.client;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.youcruit.textkernel.client.objects.Profile;
 
 public class TextKernelINTTest {
 
-    public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
 	if(args.length!=5){
 	    System.out.println("Usage: java TextKernelINTTest url username account password path_to_cv");
 	}
-	TextKernelClient tkclient = new TextKernelClient(args[0], args[1], args[2], args[3]);
-	Profile p = tkclient.getProfile(new File(args[4]));
-	System.out.println(p.getRawHtml());
+	String url = args[0];
+	String username = args[1];
+	String account = args[2];
+	String password = args[3];
+	String file = args[4];
 
+	TextKernelClient tkclient = new TextKernelClient(url, username, account, password);
+	Profile p = tkclient.getProfile(new FileInputStream(new File(file)));
+	System.out.println(p.getRawHtml());
     }
 
 }
