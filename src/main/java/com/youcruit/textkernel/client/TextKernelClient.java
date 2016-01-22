8ff26ec158f2368f5bcb2e61ba009291676a7d0e
Tobias Olsson <tobias.olsson@youcruit.com>
2016-01-22 11:29:54 +0100

    Stop using deprecated stuff and java...HttpUrlConnection

--- src/main/java/com/youcruit/textkernel/client/TextKernelClient.java
+++ src/main/java/com/youcruit/textkernel/client/TextKernelClient.java
@@ -1,34 +1,26 @@
 package com.youcruit.textkernel.client;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.entity.mime.FormBodyPart;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.ContentBody;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.InputStreamBody;
-import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.HttpParams;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.youcruit.textkernel.client.objects.Profile;
 
 public class TextKernelClient {
@@ -42,63 +34,56 @@ public class TextKernelClient {
     private final String account;
     private final String password;
     private final ProfileParser p;
-    
+    private final CloseableHttpClient client;
+
+
     public TextKernelClient(final String textKernelUrl, final String username, final String account, final String password){
 	this.username = username;
 	this.account = account;
 	this.password = password;
 	this.textKernelUrl = textKernelUrl;
 	p = new ProfileParser();
-    }
-    
-    public Profile getProfile(File file) throws JsonParseException, JsonMappingException, IOException, URISyntaxException{
-	return p.parse(upload(file));
-    }
-    
-    public Profile getProfile(InputStream is) throws JsonParseException, JsonMappingException, ClientProtocolException, IOException, URISyntaxException{
-	return p.parse(upload(new InputStreamBody(is, "")));
-    }
-    
-    public Profile getProfile(URI uri) throws JsonParseException, JsonMappingException, MalformedURLException, IOException, URISyntaxException{
-	return getProfile(uri.toURL().openStream());
+	client = HttpClients.createDefault();
     }
 
-    private InputStream upload(ContentBody content) throws ClientProtocolException, IOException, URISyntaxException{
-	DefaultHttpClient client = getClientInstance();
+    public Profile getProfile(File file) throws IOException, URISyntaxException{
+	try (InputStream fis = new FileInputStream(file)) {
+	    return getProfile(fis);
+	}
+    }
+
+    public Profile getProfile(InputStream is) throws IOException, URISyntaxException{
+	    return p.parse(upload(is));
+    }
+
+    public Profile getProfile(URI uri) throws IOException, URISyntaxException{
+	HttpGet httpGet = new HttpGet(uri);
+	HttpEntity entity = client.execute(httpGet).getEntity();
+	try (InputStream dis = entity.getContent()) {
+	    return getProfile(dis);
+	}
+    }
+
+    private byte[] upload(InputStream content) throws IOException, URISyntaxException{
 	HttpContext localContext = new BasicHttpContext();
 	URI upload_url = new URI(textKernelUrl);
 	HttpPost upload = new HttpPost(upload_url);
 
-	MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-	entity.addPart(FORM_PART_NAME_FILE, content);
-	entity.addPart(new FormBodyPart(FORM_PART_NAME_USERNAME, new StringBody(username)));
-	entity.addPart(new FormBodyPart(FORM_PART_NAME_ACCOUNT, new StringBody(account)));
-	entity.addPart(new FormBodyPart(FORM_PART_NAME_PASSWORD, new StringBody(password)));
+	HttpEntity entity = MultipartEntityBuilder.create().setMode(HttpMultipartMode.BROWSER_COMPATIBLE).
+		addBinaryBody(FORM_PART_NAME_FILE, content, ContentType.APPLICATION_OCTET_STREAM, "").
+		addTextBody(FORM_PART_NAME_USERNAME, username).
+		addTextBody(FORM_PART_NAME_ACCOUNT, account).
+		addTextBody(FORM_PART_NAME_PASSWORD, password).
+		build();
 	upload.setEntity(entity);
 	HttpResponse response = client.execute(upload, localContext);
-	
+
 	int status = response.getStatusLine().getStatusCode();
 	if(status != HttpStatus.SC_OK){
 	    String responseString = EntityUtils.toString(response.getEntity());
 	    throw new IOException("TextKernel returned a non expected response code: " + status + " \nResponse was:\n " + responseString);
 	}
-	
-	HttpEntity responseEntity = response.getEntity();
-	return responseEntity.getContent();
-    }
-    
-    private InputStream upload(File file) throws IOException, URISyntaxException {
-	FileBody fileBody = new FileBody(file, "application/octet-stream");
-	return upload(fileBody);
-    }
 
-    private DefaultHttpClient getClientInstance() {
-
-	DefaultHttpClient client = new DefaultHttpClient();
-	ClientConnectionManager mgr = client.getConnectionManager();
-
-	HttpParams params = client.getParams();
-	client = new DefaultHttpClient(new ThreadSafeClientConnManager(mgr.getSchemeRegistry()), params);
-	return client;
+	return EntityUtils.toByteArray(response.getEntity());
     }
 }
